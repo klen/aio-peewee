486eefcf0eecee8ec20f6a4303ebf9e938530257
Kirill Klenov <horneds@gmail.com>
2020-10-02 14:10:42 +0300

    build(version): 0.0.0 -> 0.0.1

--- aiopeewee.py
+++ aiopeewee.py
@@ -8,7 +8,7 @@ from playhouse import db_url, pool, cockroachdb as crdb
 from playhouse.sqlite_ext import SqliteExtDatabase
 
 
-version = "0.0.0"
+version = "0.0.1"
 
 _ctx = {
     'closed': ContextVar('closed'),
